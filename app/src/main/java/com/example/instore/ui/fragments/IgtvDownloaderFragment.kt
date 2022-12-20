f1cc787d9485e68af7a46cb4a9d479c06d030887
Abhi <abhishekvelekar79200@gmail.com>
2022-12-20 11:55:46 +0530

    Igtv Downloader fragment created

---
+++ app/src/main/java/com/example/instore/ui/fragments/IgtvDownloaderFragment.kt
@@ -0,0 +1,7 @@
+package com.example.instore.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.instore.R
+
+class IgtvDownloaderFragment : Fragment(R.layout.fragment_igtv_downloader) {
+}
