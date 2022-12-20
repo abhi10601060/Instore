6060099fa3622d24656ad93796168387257fc178
Abhi <abhishekvelekar79200@gmail.com>
2022-12-20 12:31:09 +0530

    Content view pager added to mainActivity

--- app/src/main/java/com/example/instore/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/instore/ui/activities/MainActivity.kt
@@ -2,11 +2,50 @@ package com.example.instore.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.get
+import androidx.viewpager2.widget.ViewPager2
 import com.example.instore.R
+import com.example.instore.adapter.ContentViewPagerAdapter
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar : MaterialToolbar
+    lateinit var contentTabs : TabLayout
+    lateinit var viewPager : ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        contentTabs = findViewById(R.id.content_tabLayout)
+        viewPager = findViewById(R.id.contents_viewPager)
+
+        setSupportActionBar(toolbar)
+
+        val contentViewPager = ContentViewPagerAdapter(supportFragmentManager , lifecycle)
+        viewPager.adapter = contentViewPager
+
+        contentTabs.setOnTabSelectedListener(object  : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tab?.position?.let { viewPager.setCurrentItem(it) }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                contentTabs.selectTab(contentTabs.getTabAt(position))
+            }
+        })
+
     }
 }
