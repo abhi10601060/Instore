f2053f757fcc0798ab9c6347c9adba06508f4107
Abhi <abhishekvelekar79200@gmail.com>
2022-12-20 11:51:21 +0530

    Reel Downloader Fragment created

---
+++ app/src/main/java/com/example/instore/ui/fragments/ReelDownloaderFragment.kt
@@ -0,0 +1,13 @@
+package com.example.instore.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.instore.R
+
+class ReelDownloaderFragment : Fragment(R.layout.fragment_reel_downloader) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
