5e3d442a0b1705d5e0cc3081b91c695deef17441
Abhi <abhishekvelekar79200@gmail.com>
2022-12-22 21:32:58 +0530

    image loading from posts functionality added

---
+++ app/src/main/java/com/example/instore/adapter/StoredPostAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.instore.adapter
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.instore.R
+import java.io.File
+
+class StoredPostAdapter(private var posts: List<File>, private val context: Context): RecyclerView.Adapter<StoredPostAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.post_layout , parent , false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val post = posts.get(position)
+
+        holder.postName.setText(post.name)
+
+        val bitmap = BitmapFactory.decodeFile(post.absolutePath)
+        holder.image.setImageBitmap(bitmap)
+
+    }
+
+    override fun getItemCount(): Int {
+        return posts.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val image = itemView.findViewById<ImageView>(R.id.post_image)
+        val postName = itemView.findViewById<TextView>(R.id.post_name)
+    }
+}
--- app/src/main/java/com/example/instore/ui/activities/StorageActivity.kt
+++ app/src/main/java/com/example/instore/ui/activities/StorageActivity.kt
@@ -1,9 +1,14 @@
 package com.example.instore.ui.activities
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.example.instore.R
 import com.example.instore.adapter.ContentViewPagerAdapter
@@ -22,6 +27,7 @@ class StorageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_storage)
 
         initViews()
+        askPermission()
 
         setSupportActionBar(toolbar)
         setupViewPagerWithTablayout()
@@ -58,5 +64,27 @@ class StorageActivity : AppCompatActivity() {
         toolbar = findViewById(R.id.toolbar_storage_activity)
         tabLayout = findViewById(R.id.stored_activity_tablayout)
         viewPager = findViewById(R.id.storage_viewPager)
+    }
+
+    fun askPermission() {
+        if (ContextCompat.checkSelfPermission(this , Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE) , 100)
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+
+            100 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "Read Permission granted...", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
